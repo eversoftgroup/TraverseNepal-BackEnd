cc785997f0859ba7ff585d4d363e2ff21b381299
Bikalpa Dhakal <theoctober19th@gmail.com>
2019-06-15 17:45:47 +0545

    itinerary problem fixed

--- src/main/java/com/eversoft/traverse/model/Itinerary.java
+++ src/main/java/com/eversoft/traverse/model/Itinerary.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="itineraryList")
-public class Itinerary {
+public class Itinerary implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
